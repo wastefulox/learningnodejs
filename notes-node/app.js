5cf41dfdbbff385f27ced6a8cf9795c95d8f2b91
mat sturdevant <mathew.sturdevant@my.strayer.edu>
2018-08-30 07:21:42 -0400

    updates on require your own file

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,13 +1,19 @@
-console.log('Starting App.');
+console.log('Starting app.js.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
-// console.log(user.username);
+console.log(notes.add(1,8));
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err){
-  if (err) {
-    console.log('unable to write file');
-  }
-});
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo();
+// // console.log(user.username);
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! ? You are ${notes.age}`, function (err){
+//   if (err) {
+//     console.log('unable to write file');
+//   }
+// });
